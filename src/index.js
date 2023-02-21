0e5a0e1a34cab573a222283f39fe66dd12848196
Olexander <cmurtimur@gmail.com>
2023-02-21 14:14:25 +0200

    qwrgesrt

--- src/index.js
+++ src/index.js
@@ -56,29 +56,41 @@ const foodRef = document.querySelector(".js-gallery")
 const markup = galleryItemTpl(food)
 
 foodRef.insertAdjacentHTML('beforeend', markup)
-
 const switcher = document.querySelector(".theme-switch__toggle")
 
-switcher.addEventListener('change',switcherCheced )
-const value = null
-localStorage.setItem('comment',value)
+switcher.addEventListener('change', switcherCheced )
 
+// const value = null
+// localStorage.setItem('comment',value)
+
+// 1. Отримуємо дані з localstorage
+const theme = localStorage.getItem('currentTheme'); 
+
+// колбек функція яка тоблить класи і зберігає поточний клас в локальному сховищі
 function switcherCheced () {
-    const theme = localStorage.getItem('theme');
-const value = switcher.classList
-localStorage.setItem('comment',value)
+    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('light-theme');
 
-  
-    if(theme === theme-switch__toggle){
-        document.body.classList.add('.dark-theme')
+    // const theme = localStorage.getItem('theme');
+const value = document.body.classList; // це псевдомасив, тому потрібно обрати [0] індекс, 
+// але в тебе там якийсь клас на боді ще є fs, тому [1] індекс
+console.log(value);
+localStorage.setItem('currentTheme',value[1])
+}
+
+//  якщо теми в localstorage жодної немає то зберігаємо там світлу тему
+// і додаємо на body необхідний клас, в іншому випадку додаємо те що в 
+// localstorage
+    if(theme === null){
+        localStorage.setItem('currentTheme', 'light-theme');
+        document.body.classList.add('light-theme')
        
-    }
-else{
-    document.body.classList.remove('.dark-theme')
-    localStorage.remove('comment')
-    const value = null
-localStorage.setItem('comment',value)
+    } else
+{
+    document.body.classList.add(theme);
+};
 
-}
-   
-}
+// якщо тема в localstorage темна то в атрибут checked записуємо true 
+if (theme === 'dark-theme') {
+    switcher.checked = true;
+};
